7efc249d672732422f2c08b6ac9c58910b3af3e0
Kevin Me <chekmanirov@gmail.com>
2014-11-08 15:32:53 -0500

    reducing ping frequency

--- public/js/scripts/index.js
+++ public/js/scripts/index.js
@@ -1,19 +1,12 @@
 var socket = io.connect('/');
-
-//require(['dojo/dom', 'dojo/date/locale', 'dojo/domReady!'], function(dom) {
-
-
-//	    function dateFormat(date, format) { return dojo.date.locale.format( date, {selector: 'date', datePattern:format });}
-	    function strip_id(key, value)  { return key == '_id' ? undefined : value; }
+pingServer(socket); //initial ping - heroku will close connection if no ping within 30s of connection
+function strip_id(key, value)  { return key == '_id' ? undefined : value; }
 
 	
 socket.on('all', function (data) {
-	
-document.getElementById('console').innerHTML = '<pre> data: ' + JSON.stringify(data, strip_id, 2) + '</pre>'; 
+	document.getElementById('console').innerHTML = '<pre> data: ' + JSON.stringify(data, strip_id, 2) + '</pre>'; 
 });
-/*onefour = dom.byId('onefour');
-onefour.innerHTML = '<pre> data: ' + JSON.stringify(data, strip_id, 2) + '</pre>'; 
-});*/
+
 
 function pingServer(socket)
 	{
@@ -21,9 +14,10 @@ function pingServer(socket)
 	socket.emit('ping');
 	}
 
+
+// ping server every 30s (heroku timeout is 55s)
 setInterval(function ()
 	{
 	pingServer(socket);
-	}, 15000);
+	}, 30000);
 	    
-//	    });
